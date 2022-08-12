754273c88e89e14f6a35b19d87f506a2598448eb
Trinsdar <30245301+Trinsdar@users.noreply.github.com>
2022-08-12 15:31:03 -0400

    fixed data loading

--- src/main/java/net/devtech/arrp/mixin/LifecycledResourceManagerImplMixin.java
+++ src/main/java/net/devtech/arrp/mixin/LifecycledResourceManagerImplMixin.java
@@ -7,29 +7,32 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executor;
 
 import net.devtech.arrp.api.RRPEvent;
+import net.devtech.arrp.api.RRPInitEvent;
 import net.devtech.arrp.api.RuntimeResourcePack;
 import net.devtech.arrp.util.IrremovableList;
+import net.minecraft.resource.LifecycledResourceManagerImpl;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.ModLoader;
+import net.minecraftforge.fml.loading.FMLEnvironment;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
-import net.minecraft.resource.ReloadableResourceManagerImpl;
 import net.minecraft.resource.ResourcePack;
 import net.minecraft.util.Unit;
 
-@Mixin (ReloadableResourceManagerImpl.class)
-public abstract class ReloadableResourceManagerImplMixin {
-	private static final Logger ARRP_LOGGER = LogManager.getLogger("ARRP/ReloadableResourceManagerImplMixin");
+@Mixin (LifecycledResourceManagerImpl.class)
+public abstract class LifecycledResourceManagerImplMixin {
 
-	@ModifyVariable(method = "reload",
-			at = @At (value = "HEAD"), argsOnly = true)
-	private List<ResourcePack> registerARRPs(List<ResourcePack> packs, Executor prepareExecutor,
-			Executor applyExecutor,
-			CompletableFuture<Unit> initialStage,
-			List<ResourcePack> packs0) throws ExecutionException, InterruptedException {
+	private static boolean LOADED = false;
+	private static final Logger ARRP_LOGGER = LogManager.getLogger("ARRP/LifecycledResourceManagerImplMixin");
+
+	@ModifyVariable(method = "<init>",
+			at = @At (value = "HEAD"),
+			argsOnly = true)
+	private List<ResourcePack> registerARRPs(List<ResourcePack> packs) throws ExecutionException, InterruptedException {
 		//ARRP.waitForPregen();
 
 		ARRP_LOGGER.info("ARRP register - before vanilla");
